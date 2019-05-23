f49b086e0deb1fadfdde5676d93932fb379195bb
jl051 <jl051@192.168.0.30>
2019-05-22 22:06:53 -0600

    Hacer que encienda las luces por consola

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <stdlib.h>
-#include <string>
 #include "SerialPort.h"
 
 using namespace std;
@@ -10,55 +9,90 @@ char *port = "\\\\.\\COM3";
 char incoming[MAX_DATA_LENGTH];
 
 
+
 int main(){
 
 	int opcion;
 
-        //instanciamos objeto arduino
+                //instanciamos objeto arduino-----------------------------------------------------------
 		SerialPort arduino(port);
 
-		//verificamos conexion
-		if(arduino.isConnected()){
+		//verificamos conexion-----------------------------------------------------------------
+        if(arduino.isConnected()){
 
-			cout<<"Conexion correcta!!!\n";
+            cout<<"Conexion correcta!!!\n";
 
-        //AGREGAMOS OPCIONES DE MENU SOLO DE HABER UNA CONEXION EXITOSA -------------------------------------
+                        //AGREGAMOS OPCIONES DE MENU SOLO DE HABER UNA CONEXION EXITOSA
 		do{
-			cout<<"----------------------------Menu--------------------------------"<<endl;
-			cout<<"1. opcion 1"<<endl;
-			cout<<"2. opcion 2"<<endl;
-			cout<<"3. opcion 3"<<endl;
-			cout<<"4. opcion 4"<<endl;
-			cout<<"5. Salir"<<endl;
+                    cout<<"----------------------------Menu--------------------------------"<<endl;
+                    cout<<"1. opcion 1"<<endl;
+                    cout<<"2. opcion 2"<<endl;
+                    cout<<"3. opcion 3"<<endl;
+                    cout<<"4. Salir"<<endl;
 
 
-			cin>>opcion;
+                    cin>>opcion;
 
-			switch(opcion){
+                if(opcion==1){
 
-			case 1: break;
-			case 2: break;
-			case 3: break;
-			case 4: break;
-			case 5: cout<<"Saliendo del Menu"; break;
-			default: break;
-			}
+                            char respuesta = 'n';
+                            string comando;
 
-		}while(opcion!=5);
+                            while(arduino.isConnected() && respuesta == 'n'){
+
+                                cout<<"Ingrese la funcion que desea Ejecutar (ON/OFF)"<<endl;
+
+                                cin>>comando;
+
+                                //AGREGAMOS UN ARRAY QUE CRECERA POR CADA VUELTA
+                                char *matriz = new char[comando.size() +1];
+
+                                copy(comando.begin(), comando.end(), matriz);
+
+                                matriz[comando.size()]='\n';
+
+                                arduino.writeSerialPort(matriz, MAX_DATA_LENGTH);
+                                arduino.readSerialPort(outPut, MAX_DATA_LENGTH);
+
+                                cout<<outPut;
+
+                            delete [] matriz;
+
+                            cout<<"Desea realizar otra funcion (Si = s/ No = n)"<<endl;
+                            cin>>respuesta;
+
+                        }
+
+                    }else if(opcion==2){
+
+                            cout<<"opcion 2"<<endl;
+
+                    }else if(opcion==3){
+
+                            cout<<"opcion 3"<<endl;
+
+                    }else{
+
+                        cout<<"Opcion no valida"<<endl;
+                    }
+
+		}while(opcion!=4);
 
         system("pause");
 
-	return 0;
 
-		}else{//EN CASO DE QUE FALLE LA CONEXION -----------------------------------------------
+
+		}else{
 
 			cout<<"Conexion incorrecta!!!!";
 
                         system("pause");
 
-                        return 0;
+
 		}
 		//-------------------------------------------------------------------------------------
-
+	return 0;
 }
+
+
 
