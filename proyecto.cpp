bca38a3569b355e624bea0e78daf5d7465871cb6
luixgarcia11 <47509981+luixgarcia11@users.noreply.github.com>
2019-05-25 02:49:58 -0600

    Update proyecto.cpp

--- proyecto.cpp
+++ proyecto.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 void estado();
 void encendido();
+void fecha();
 
 
 int main()
@@ -59,4 +60,36 @@ void encendido(){
 	// Encener luz.
 	cout << "Enviando: " << Luz_ON << endl; // Muestra en pantalla textos.
 	Puerto->WriteData(Luz_ON, sizeof(Luz_ON)-1); // Envía al puerto el texto "Luz_ON"
+}
+
+
+void fecha(){
+	FILE    *file;
+	time_t  tiempo;
+	struct  tm  *timelocal;
+	char    out_time[128], d[25];
+
+	tiempo = time(0);
+	timelocal = localtime(&tiempo);
+	strftime(out_time, sizeof(out_time), "%d/%m/%y  %H:%M:%S", timelocal);
+
+	if (((file = fopen(NOM_FILE, "a")) == NULL) || ((file = fopen(NOM_FILE, "r")) == NULL))
+		file = fopen(NOM_FILE, "w");
+
+	if ((file = fopen(NOM_FILE, "a")) == NULL)
+	{
+		perror(NOM_FILE);
+		exit(1);
+	}
+
+	fprintf(file, "%s\n", out_time);
+	fclose(file);
+
+	if ((file = fopen(NOM_FILE, "r")) == NULL)
+	{
+		perror(NOM_FILE);
+		exit(1);
+	}
+
+	fclose(file);
 }
